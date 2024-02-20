2d49413ad9782c234ca7f161af2b40a47add7135
Ellen Kraffmiller <ekraffmiller@hmdc.harvard.edu>
2024-02-20 16:35:47 -0500

    remove unused code in App.tsx

--- react-design-system-example/src/App.tsx
+++ react-design-system-example/src/App.tsx
@@ -1,22 +1,14 @@
 import './App.css'
 import { ApiConfig } from '@iqss/dataverse-client-javascript/dist/core'
 import {DataverseApiAuthMechanism} from "@iqss/dataverse-client-javascript/dist/core/infra/repositories/ApiConfig";
-import {getAllDatasetPreviews} from "@iqss/dataverse-client-javascript";
 import {DatasetTable} from "./datasets/dataset-table/DatasetTable.tsx";
-ApiConfig.init('http://localhost:8000/api/v1', DataverseApiAuthMechanism.API_KEY,'your api key' )
+ApiConfig.init('http://localhost:8000/api/v1', DataverseApiAuthMechanism.API_KEY,'<API TOKEN>' )
 
 
 
 function App() {
-  //const [count, setCount] = useState(0)
-
-    getAllDatasetPreviews.execute().then((datasetPreviewSubset) => {
-        console.log('Dataset List:', JSON.stringify(datasetPreviewSubset));
-    });
   return (
-    <>
      <DatasetTable />
-    </>
   )
 }
 
