a99fc140ddaa325cc8949888eb5333210d958189
Ellen Kraffmiller <ekraffmiller@hmdc.harvard.edu>
2024-02-26 15:12:23 -0500

    App.tsx: read api token from environment var

--- react-design-system-example/src/App.tsx
+++ react-design-system-example/src/App.tsx
@@ -2,7 +2,7 @@ import './App.css'
 import { ApiConfig } from '@iqss/dataverse-client-javascript/dist/core'
 import {DataverseApiAuthMechanism} from "@iqss/dataverse-client-javascript/dist/core/infra/repositories/ApiConfig";
 import {DatasetTable} from "./datasets/dataset-table/DatasetTable.tsx";
-ApiConfig.init('http://localhost:8000/api/v1', DataverseApiAuthMechanism.API_KEY,'<API TOKEN>' )
+ApiConfig.init('http://localhost:8000/api/v1', DataverseApiAuthMechanism.API_KEY, import.meta.env.DATAVERSE_API_TOKEN )
 
 
 
