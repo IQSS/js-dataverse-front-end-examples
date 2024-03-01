9e4922f7db6c9534158ff2c5e874f511e8cb7f30
German Gonzalo Saracca <gersaracca@gmail.com>
2024-03-01 15:23:57 -0300

    Add layout component with header, main, and footer styles

---
+++ react-design-system-example/src/layout/index.tsx
@@ -0,0 +1,18 @@
+import { Outlet } from "react-router-dom";
+import styles from "./index.module.css";
+
+export const Layout = () => {
+  return (
+    <>
+      <header className={styles.header}>
+        <h1>React Design System Example</h1>
+      </header>
+      <main className={styles.main}>
+        <Outlet />
+      </main>
+      <footer className={styles.footer}>
+        <p>Design System Example &copy; {new Date().getFullYear()}</p>
+      </footer>
+    </>
+  );
+};
