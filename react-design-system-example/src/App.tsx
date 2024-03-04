91fca6cb278cc4f08777dc61b996afdf72665d12
Ellen Kraffmiller <ekraffmiller@hmdc.harvard.edu>
2024-03-04 15:24:06 -0600

    Set API Key in App.tsx

--- react-design-system-example/src/App.tsx
+++ react-design-system-example/src/App.tsx
@@ -2,11 +2,8 @@ import { ApiConfig } from '@iqss/dataverse-client-javascript/dist/core'
 import { DataverseApiAuthMechanism } from '@iqss/dataverse-client-javascript/dist/core/infra/repositories/ApiConfig'
 import Router from './router'
 
-ApiConfig.init(
-  'http://localhost:8000/api/v1',
-  DataverseApiAuthMechanism.API_KEY,
-  import.meta.env.DATAVERSE_API_TOKEN
-)
+// Initialize the API configuration
+ApiConfig.init('http://localhost:8000/api/v1', DataverseApiAuthMechanism.API_KEY, '<API_KEY>')
 
 function App() {
   return <Router />
